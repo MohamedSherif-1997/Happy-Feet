ce00e4f95d232609c2f09f08d30606592e642cbc
Sherif <sherifkhan1997@gmail.com>
2020-07-15 00:07:29 +0530

    completed-Product Cards

--- src/Components/products.js
+++ src/Components/products.js
@@ -1,9 +1,115 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class Products extends Component {
-  render() {
-    return <div>Products</div>;
-  }
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import Chip from "@material-ui/core/Chip";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 300,
+    marginBottom: "10px",
+    marginLeft: "15px",
+  },
+  media: {
+    height: 100,
+    width: 200,
+    paddingTop: "20px",
+    paddingLeft: "20px",
+  },
+  cardContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    width: "100%",
+  },
+  image: {
+    paddingTop: "20px",
+    paddingLeft: "25px",
+    width: "300px",
+  },
+  rating: {
+    backgroundColor: "green",
+    color: "white",
+  },
+  discount: {
+    color: "green",
+    fontSize: "15px",
+    paddingLeft: "3px",
+  },
+  price: {
+    paddingLeft: "3px",
+    fontSize: "10px",
+  },
+  buyButton: {
+    color: "black",
+    backgroundColor: "white",
+    border: "2px solid green",
+  },
+  button: {
+    paddingLeft: "90px",
+  },
+});
+
+export default function Products(props) {
+  const products = props.products;
+  const classes = useStyles();
+  return (
+    <div container className={classes.cardContainer}>
+      {products.length > 0 ? (
+        products.map((product, index) => (
+          <Card className={classes.root} key={index}>
+            <CardActionArea className={classes.image}>
+              <CardMedia
+                className={classes.media}
+                image={product.image}
+                title={product.name}
+              />
+            </CardActionArea>
+            <CardContent>
+              <Typography gutterBottom component="h4">
+                {product.name}
+              </Typography>
+              <Typography>
+                <Chip
+                  variant="outlined"
+                  size="small"
+                  label={product.rating}
+                  icon={<StarRateIcon style={{ color: "white" }} />}
+                  className={classes.rating}
+                />
+                <span className={classes.button}>
+                  <Button size="small" className={classes.buyButton}>
+                    Buy
+                  </Button>
+                </span>
+              </Typography>
+              <Typography>
+                <p>
+                  &#8377;
+                  {(product.price * product.discount) / 100}
+                  <s className={classes.price}>{`${product.price}`}</s>
+                  <span className={classes.discount}>
+                    {product.discount}&#x25; off
+                  </span>
+                </p>
+              </Typography>
+            </CardContent>
+          </Card>
+        ))
+      ) : (
+        <Typography>No Data Available</Typography>
+      )}
+    </div>
+  );
 }
 
-export default Products;
+Products.propTypes = {
+  products: PropTypes.array.isRequired,
+};
--- src/Container/DashboardContainer/index.js
+++ src/Container/DashboardContainer/index.js
@@ -2,9 +2,21 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core";
 
+//Components
 import Filters from "../../Components/filters";
 import Sort from "../../Components/sort";
 import Products from "../../Components/products";
+
+// Images
+import nick1 from "../../images/shoes/nike1.png";
+import nick2 from "../../images/shoes/nike2.jpeg";
+import nick3 from "../../images/shoes/nike3.jpeg";
+import puma1 from "../../images/shoes/puma1.jpeg";
+import puma2 from "../../images/shoes/puma2.jpeg";
+import leeCooper1 from "../../images/shoes/leeCooper1.jpg";
+import leeCooper2 from "../../images/shoes/leeCooper2.jpeg";
+import leeCooper3 from "../../images/shoes/leeCooper3.jpg";
+import reeBok from "../../images/shoes/reeBok1.jpg";
 
 const style = {
   mainFlex: {
@@ -34,6 +46,81 @@ const style = {
   },
 };
 
+const products = [
+  {
+    name: "Nike React Element 87 Anthracite Black",
+    image: nick1,
+    price: 1200,
+    discount: 50,
+    brand: "Nike",
+    rating: 4.2,
+  },
+  {
+    name: "Nie Announces Special Edition",
+    image: nick2,
+    price: 800,
+    discount: 50,
+    brand: "Nike",
+    rating: 4.2,
+  },
+  {
+    name: "Nike VTR sneakers Red/Black",
+    image: nick3,
+    price: 450,
+    discount: 50,
+    brand: "Nike",
+    rating: 4.2,
+  },
+  {
+    name: "Tan Men Lee Cooper Shoes",
+    image: leeCooper1,
+    price: 1200,
+    discount: 50,
+    brand: "LeeCooper",
+    rating: 4.2,
+  },
+  {
+    name: "Brown men Leecooper Shoes",
+    image: leeCooper2,
+    price: 800,
+    discount: 50,
+    brand: "LeeCooper",
+    rating: 4.2,
+  },
+  {
+    name: "Lee Cooper Men's Leather Sneakers",
+    image: leeCooper3,
+    price: 450,
+    discount: 50,
+    brand: "LeeCooper",
+    rating: 3.8,
+  },
+  {
+    name: "Reebok shoes for Men – Designed",
+    image: reeBok,
+    price: 1200,
+    discount: 50,
+    brand: "Reebok",
+    rating: 4.6,
+  },
+  {
+    name: "Puma mens-Blue/Black",
+    image: puma1,
+    price: 800,
+    discount: 50,
+    brand: "Puma",
+    rating: 3.0,
+  },
+  {
+    name: "puma suede shoes",
+    image: puma2,
+    price: 450,
+    discount: 50,
+    brand: "Puma",
+    rating: 4.0,
+  },
+];
+
 class DashboardContainer extends Component {
   render() {
     const { classes } = this.props;
@@ -48,7 +135,7 @@ class DashboardContainer extends Component {
               <Sort />
             </div>
             <div className={classes.product}>
-              <Products />
+              <Products products={products} />
             </div>
           </div>
         </div>
