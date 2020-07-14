bab267844ee81792661029d44d42a0d14e298306
Sherif <sherifkhan1997@gmail.com>
2020-07-15 01:49:31 +0530

    completed-sort

--- src/Components/sort.js
+++ src/Components/sort.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import Button from "@material-ui/core/Button";
 
 class Sort extends Component {
   render() {
-    return <div>Sort</div>;
+    const {
+      handlePriceHigh,
+      handlePriceLow,
+      handleRelevence,
+      disabled,
+    } = this.props;
+    return (
+      <div>
+        Sort By:
+        <Button onClick={handleRelevence} disabled={disabled}>
+          Relevence
+        </Button>
+        <Button onClick={handlePriceLow}>Price Low-High</Button>
+        <Button onClick={handlePriceHigh}>Price High-Low</Button>
+      </div>
+    );
   }
 }
+
+Sort.propTypes = {
+  disabled: PropTypes.bool.isRequired,
+  handlePriceHigh: PropTypes.func.isRequired,
+  handlePriceLow: PropTypes.func.isRequired,
+  handleRelevence: PropTypes.func.isRequired,
+};
 
 export default Sort;
